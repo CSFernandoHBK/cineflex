849f3f599da76cb7773157b8221192d15929b03b
Fernando <fernandotam2@hotmail.com>
2022-10-03 22:49:15 -0300

    files created

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import CineMain from "./components/CineMain"
+import GlobalStyle from "./GlobalStyles";
+
+export default function Tela(){
+    return(
+        <>
+            <GlobalStyle/>
+            <CineMain/>
+        </>
+    );
+}
---
+++ src/components/CineMain.js
@@ -0,0 +1,9 @@
+export default function CineMain() {
+    return(
+        <>
+            <p>Olá mundo</p>
+            <div>Olá de novo</div>
+        </>
+    )
+
+}
